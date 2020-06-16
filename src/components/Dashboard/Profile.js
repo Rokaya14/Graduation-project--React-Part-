45dc3b9601bcc904643c796d8e166f9ea1acfa36
rokaya magdi <rokaiamagdi@gmail.com>
2020-06-16 19:27:16 +0200

    profile navbar edit

--- src/components/Dashboard/Profile.js
+++ src/components/Dashboard/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../Stylesheets/Dashboardstyle.css';
 import SidebarD from './SidebarD';
+import NavebarD from './NavebarD';
 
 class Profile extends React.Component {
     constructor(props){  
@@ -31,31 +32,7 @@ class Profile extends React.Component {
                 <SidebarD/>
                 
                 <div id="content" >  
-                    <nav className= " navbar navbar-expand-lg navbar-dark bg-dark" >
-                        <div className="container-fluid">
-                            <button type="button" id="sidebarCollapse" className="btn btn-info">
-                                <i className="fas fa-align-left"></i>
-                                <span>Toggle Sidebar</span>
-                            </button>
-                            <button className="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <i className="fas fa-align-justify"></i>
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul className="nav navbar-nav ml-auto">
-                                    <li className="nav-item active">
-                                        <a className="navbar-brand" href="/Dashboard">Dashboard</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="/Profile">UserName</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="/Login">Logout</a>
-                                    </li>                        
-                                </ul>
-                                
-                            </div>
-                        </div>
-                    </nav>
+                    <NavebarD/>
                     <div className="styleform">
                         <form  onSubmit={this.handleUpdate}>
 
